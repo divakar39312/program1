5e89ffec76301946b02cbc1dd4099eea6a2ac728
Divakar <divakar39312@gmail.com>
2022-08-04 13:01:54 +0530

    [Divakar]:UC2 added

--- src/com/Bridgelabz/Tictactoe.java
+++ src/com/Bridgelabz/Tictactoe.java
@@ -13,15 +13,29 @@ public class Tictactoe {
      * UC1 This method is assigning space to board array
      */
     static void startGame() {
-    	for(int i = 1;i<=9;i++) {
+    	for(int i = 1;i <= 9;i++) {
     		board[i] = ' ';
     	}		
 	}
-    
+    /**
+     * UC2 Taking playing symbol from user as X or O
+     */
+    static void playerInput() {
+		System.out.println("player 1 please select the symbbol(x/o):");
+		player1=sc.next().charAt(0);
+		if(player1 =='X' || player1=='O') {
+			System.out.println("player 1 symbol :" + player1);
+		}else {
+			System.out.println("invalid symbol please enter x or o");
+			playerInput();
+		}
+	}
 
 	public static void main(String[] args) {
 		System.out.println("welcome to tictactoe simulation");
 		startGame();
+		
+		playerInput();
 	}
 
 }
